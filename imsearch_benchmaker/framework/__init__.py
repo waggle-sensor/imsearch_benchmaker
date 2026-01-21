f98809d0f278310b105fa5e972468302c7247222
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-01-21 14:58:58 -0600

    Refactor post-processing imports to replace calculate_clipscore with calculate_similarity_score

--- imsearch_benchmaker/framework/__init__.py
+++ imsearch_benchmaker/framework/__init__.py
@@ -7,7 +7,7 @@ from .judge import Judge, JudgeAdapterRegistry
 from .scoring import Similarity, SimilarityAdapterRegistry
 from .preprocess import build_images_jsonl, build_seeds_jsonl
 from .query_plan import build_query_plan, load_annotations, TagOverlapQueryPlan
-from .postprocess import generate_dataset_summary, calculate_clipscore, huggingface
+from .postprocess import generate_dataset_summary, calculate_similarity_score, huggingface
 from .config import BenchmarkConfig, DEFAULT_BENCHMARK_CONFIG, VisionConfig, JudgeConfig, SimilarityConfig
 from .pipeline import (
     run_preprocess,
@@ -31,7 +31,7 @@ __all__ = [
     "load_annotations",
     "TagOverlapQueryPlan",
     "generate_dataset_summary",
-    "calculate_clipscore",
+    "calculate_similarity_score",
     "huggingface",
     "BenchmarkConfig",
     "DEFAULT_BENCHMARK_CONFIG",
